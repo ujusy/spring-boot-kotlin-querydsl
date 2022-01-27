494b75c0195dd41289d823e0f297d14596ad1f85
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-27 14:05:08 +0900

    Initial commit

---
+++ src/main/kotlin/com/uju/springbootkotlinquerydsl/SpringBootKotlinQuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.uju.springbootkotlinquerydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinQuerydslApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinQuerydslApplication>(*args)
+}
---
+++ src/test/kotlin/com/uju/springbootkotlinquerydsl/SpringBootKotlinQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uju.springbootkotlinquerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinQuerydslApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
