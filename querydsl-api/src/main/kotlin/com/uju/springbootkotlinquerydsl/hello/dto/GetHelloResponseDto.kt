3102cf32d41936b9ebc397c87fdb4166347bd99d
ujusy <lsyeon1997@ajou.ac.kr>
2022-02-10 23:49:34 +0900

    fix: getHelloResponseDto

--- querydsl-api/src/main/kotlin/com/uju/springbootkotlinquerydsl/hello/dto/GetHelloResponseDto.kt
+++ querydsl-api/src/main/kotlin/com/uju/springbootkotlinquerydsl/hello/dto/GetHelloResponseDto.kt
@@ -1,6 +1,6 @@
 package com.uju.springbootkotlinquerydsl.hello.dto
 
 class GetHelloResponseDto(
-    private val id: Long,
-    private val name: String,
+    val id: Long,
+    val name: String,
 )
