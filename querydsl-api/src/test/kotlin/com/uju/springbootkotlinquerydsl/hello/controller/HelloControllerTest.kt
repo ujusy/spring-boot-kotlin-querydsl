c14b6a779c280b1210e34ac509779db3496b2d5e
ujusy <lsyeon1997@ajou.ac.kr>
2022-02-24 23:57:17 +0900

    test: kotest controller test

--- querydsl-api/src/test/kotlin/com/uju/springbootkotlinquerydsl/hello/controller/HelloControllerTest.kt
+++ querydsl-api/src/test/kotlin/com/uju/springbootkotlinquerydsl/hello/controller/HelloControllerTest.kt
@@ -2,16 +2,22 @@ package com.uju.springbootkotlinquerydsl.hello.controller
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
-@WebMvcTest(controllers = [HelloController::class])
-internal class HelloControllerTest(
-    private val mockMvc: MockMvc
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class HelloControllerTest @Autowired constructor(
+    private val mockMvc: MockMvc,
 ) : FreeSpec({
 
     "getHello()" - {
@@ -29,6 +35,25 @@ internal class HelloControllerTest(
 
             // then
             result.response.status shouldBe HttpStatus.OK.value()
+        }
+    }
+
+    "createHello()" - {
+
+        "정상적으로 생성되어야한다." {
+            // given
+            val name: String = "test"
+
+            // when
+            val result: MvcResult = mockMvc.perform(
+                post("/hello")
+                    .content("{ name: name }")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andReturn()
+
+            // then
+            println(result)
         }
     }
 })
